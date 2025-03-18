48bd0205d2116d9b7dfc03f2070821839b05a982
kassaab <abrhamkassa12@gmail.com>
2025-03-18 13:17:21 -0700

    first commit

---
+++ src/main/java/com.github.kassaab.calculator/Calculator.java
@@ -0,0 +1,18 @@
+package com.github.kassaab.calculator;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
---
+++ src/main/java/com.github.kassaab.calculator/Main.java
@@ -0,0 +1,13 @@
+//Abraham Kassa
+
+package com.github.kassaab.calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.add(5, 3);
+        System.out.println("5 + 3 = " + sum);
+
+    }
+}
