6c36f33ae2370b9936112061330011196199b0c7
kassaab <abrhamkassa12@gmail.com>
2025-03-19 13:01:35 -0700

    Calculator test added

--- src/main/java/com/github/kassaab/calculator/Calculator.java
+++ src/main/java/com/github/kassaab/calculator/Calculator.java
@@ -9,6 +9,10 @@ public class Calculator {
         return a - b;
     }
 
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
     public int divide(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("Cannot divide by zero");
